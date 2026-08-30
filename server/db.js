15eb5d8970a8b8cb2600c8a06a42b503f555e6e8
Humoyun Azimov <azimovhumoyun3444@gmail.com>
2026-08-31 00:40:55 +0500

    Support MONGODB_URI and MONGO_URI environment variables in db.js

--- server/db.js
+++ server/db.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.join(__dirname, '../.env') });
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://Kyro_Project:azimov_3444@cluster0.mgauugq.mongodb.net/premium_tools_db?retryWrites=true&w=majority&appName=Cluster0';
+const MONGO_URI = process.env.MONGO_URI || process.env.MONGODB_URI || 'mongodb+srv://Kyro_Project:azimov_3444@cluster0.mgauugq.mongodb.net/premium_tools_db?retryWrites=true&w=majority&appName=Cluster0';
 
 export const connectDB = async () => {
     try {
